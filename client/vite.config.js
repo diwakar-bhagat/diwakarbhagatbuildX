67988b7fe5ec373f4d6c6e72d9e7d4e8fad9c0a5
diwakari <diwakarbhagat.pro@gmaill.com>
2026-02-24 13:34:59 +0530

    Add chunk splitting and raise chunkSizeWarningLimit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-redux': ['react-redux', '@reduxjs/toolkit'],
+          'vendor-firebase': ['firebase/app', 'firebase/auth'],
+          'vendor-utils': ['axios'],
+        },
+      },
+    },
+  },
 })
